108332810014bdad14d33feae17eefdbbf2bcee7
marturo <vazben94@gmail.com>
2024-02-18 13:30:36 -0600

    first commit

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,13 @@
+import { Component  } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone :true ,
+  template : `<h1>Hola muindo</h1>`,
+})
+
+export class UsersLoaderComponent  {
+  constructor() { }
+
+
+}
