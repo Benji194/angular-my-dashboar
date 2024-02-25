44335a2c3d8fd34d3bb69a4a4340dc358824673b
marturo <vazben94@gmail.com>
2024-02-25 12:08:02 -0600

    to signal and computers signals

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {  toSignal } from '@angular/core/rxjs-interop';
+import type { User } from '@interfaces/req-response';
+import { TitleComponent } from '@shared/title/title.component';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [ TitleComponent , CommonModule ],
+  template: `
+    <app-title [title]="titleLabel() "></app-title>
+    @if( user() ){
+      <section>
+        <img
+          [srcset]="user()!.avatar"
+          [alt]=" user()!.first_name "
+        >
+
+        <div>
+          <h3>{{ user()!.first_name }}  {{ user()!.last_name }}</h3>
+          <p>{{ user()!.email}}</p>
+        </div>
+      </section>
+    }
+    @else{
+      <p>Cargando Información </p>
+    }
+  `,
 })
 export default class UserComponent {
+
+  // titleLabel = informacion del usuario : Tracey Ramos
+
+
+
+  private route = inject (ActivatedRoute);
+  private usersServices = inject (UsersService);
+  // public user = signal<User |  undefined> (undefined);
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(  ( { id } )=> this.usersServices.getUserById(id)   )
+    )
+  );
+
+  public  titleLabel =  computed(  () =>{
+    if (this.user()) {
+      return `Informacion del usuario :  ${ this.user()?.first_name } ${ this.user()?.last_name }`;
+    }
+
+    return 'Informacion del usuario';
+  })
+  // constructor(){
+    // this.route.params.subscribe( params => {
+    //   console.log(params);
+
+    // }
+    // )
+
+  // }
 
 }
--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ TitleComponent , CommonModule , RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
-  public userService = inject(UsersService);
+  public usersService = inject(UsersService);
+
+
 }
